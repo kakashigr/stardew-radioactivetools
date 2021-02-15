e3959d133fdf838b86cf4cdc75398288140eb08d
kakashigr <kakashigr@users.noreply.github.com>
2021-02-15 23:19:36 +0200

    sync

---
+++ RadioactiveTools/ModConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace RadioactiveTools.Framework {
+    public class ModConfig {
+        public bool UseSprinklersAsScarecrows { get; set; } = true;
+        public bool UseSprinklersAsLamps { get; set; } = true;
+        public int SprinklerRange { get; set; } = 5;
+        public int RadioactiveToolLength { get; set; } = 7;
+        public int RadioactiveToolWidth { get; set; } = 2;
+        public int RadioactiveToolCost { get; set; } = 200000;
+    }
+}
---
+++ RadioactiveTools/README.md
@@ -0,0 +1,1 @@
+# stardew-radioactivetools
